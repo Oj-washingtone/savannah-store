e32d7264c07488a53cf2acef330b8bff30964b81
Oj-washingtone <jalangowashingtone@gmail.com>
2025-08-21 16:03:25 +0300

    fix: comment out environment variable loading in main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,22 +12,19 @@
 package main
 
 import (
-	"log"
-
 	docs "github.com/Oj-washingtone/savannah-store/docs"
 	"github.com/Oj-washingtone/savannah-store/internal/api"
 	"github.com/Oj-washingtone/savannah-store/internal/database"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
 
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Failed to load the env vars: %v", err)
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatalf("Failed to load the env vars: %v", err)
+	// }
 
 	database.ConnectDB()
 	router := gin.Default()
